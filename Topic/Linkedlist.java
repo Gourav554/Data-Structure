50536af0f0e928e69e81f58c6637f60257892cf3
Gourav554 <Jgourav554@gmail.com>
2026-04-04 21:35:52 +0530

    Linkedlist

---
+++ Topic/Linkedlist.java
@@ -0,0 +1,29 @@
+package Topic;
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Linkedlist {
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+
+        head.next = second;
+        second.next = third;
+
+        // Print Linked List
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.print("NULL");
+    }
+}

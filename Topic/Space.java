9f154c9e507e8bdb52a574d68cf42bcbd850194e
Gourav554 <Jgourav554@gmail.com>
2026-04-04 21:34:02 +0530

    Space

---
+++ Topic/Space.java
@@ -0,0 +1,11 @@
+package Topic;
+
+public class Space {
+    
+}
+// Space Complexity measures how much memory (space) an algorithm uses as the input size increases.
+
+// 👉 It includes:
+
+// Input space (data given)
+// Auxiliary space (extra memory used by algorithm)

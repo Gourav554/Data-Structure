d06d4d1270cdee1425a8d02a28e65fe4dfcf32f5
Gourav554 <Jgourav554@gmail.com>
2026-04-05 19:55:50 +0530

    largest

--- Arrays/Largest_Element.java
+++ Arrays/Largest_Element.java
@@ -1,19 +1,21 @@
 
 
 public class Largest_Element {
-    public static int findlargestElementn  (int arr[])
+    public static int findlargestElementn  (int arr[]){
     int largest = arr[0];
     for(int i=0;i<arr.length;i++){
         if(arr[i]>largest){
             largest = arr[i];
         }
+    }
         return largest;
     }
 
     public static void main(String[] args) {
         int[] arr = {3, 5, 7, 2, 8};
     
-       findlargestElementn(arr);
+       int result=findlargestElementn(arr);
+       System.out.println("largest:" + result);
     }
 
 }

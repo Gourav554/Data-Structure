78f68b341e879bc6c7c64587902553f8b7321056
Gourav554 <Jgourav554@gmail.com>
2026-04-06 20:49:59 +0530

    larfgest

--- Arrays/Largest_Element.java
+++ Arrays/Largest_Element.java
@@ -1,6 +1,8 @@
 
 
 public class Largest_Element {
+
+    //not a Inbuild function
     public static int findlargestElementn  (int arr[]){
     int largest = arr[0];
     for(int i=0;i<arr.length;i++){

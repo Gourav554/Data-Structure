1b177dbbbee55df5819544294256cdeab7f7b5f0
Gourav554 <Jgourav554@gmail.com>
2026-04-07 22:08:31 +0530

    Count

---
+++ Arrays/Count_array.java
@@ -0,0 +1,15 @@
+public class Count_array {
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 7, 2, 8};
+        int[] arr1 ={2};
+        int count = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] ==arr1[0]){
+                count++;
+;            }
+            }
+        System.out.println("count of the element is: " + count);
+        }
+    }
+    
+

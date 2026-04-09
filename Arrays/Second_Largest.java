0558e78505df24b16b26164e4711271449d49500
Gourav554 <Jgourav554@gmail.com>
2026-04-09 20:08:04 +0530

    second

--- Arrays/Second_Largest.java
+++ Arrays/Second_Largest.java
@@ -1,7 +1,21 @@
 public class Second_Largest {
+
+    public static int secondlargest(int arr[]){
+        int largest = arr[0];
+        int seclargest = arr[1];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>largest){
+                largest = arr[i];
+            }
+            else if (arr[i]>seclargest &&arr[i]!=largest){
+                seclargest = arr[i];
+            }
+        }
+        return seclargest;
+    }
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        int secondLargest = findSecondLargest(arr);
-        System.out.println("The second largest element is: " + secondLargest);
+        int arr[] ={ 85,96,4,5,78,38,455,65};
+        int result = secondlargest(arr);
+        System.out.println("Second largest element is: " + result);
     }
 }

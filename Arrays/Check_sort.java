2f01cbd0edb7a5cfe7520f74ac2bc27e42602d8e
Gourav554 <Jgourav554@gmail.com>
2026-04-09 20:17:47 +0530

    sort

---
+++ Arrays/Check_sort.java
@@ -0,0 +1,18 @@
+public class Check_sort {
+    public static void main(String[] args){
+        int  arr[] = {1,2,9,4,5,6};
+        boolean sort = true;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                sort = false;
+            }
+        }
+        if(sort){
+            System.out.println("array is sorted");
+        }
+        else{
+            System.out.println("array is not sorted");
+        }
+    }
+    
+}

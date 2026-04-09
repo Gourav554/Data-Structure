a2c14c41d156949051e4cc714e47601087bc9913
Gourav554 <Jgourav554@gmail.com>
2026-04-09 20:29:11 +0530

    move zero

---
+++ Arrays/Move_Zero.java
@@ -0,0 +1,18 @@
+public class Move_Zero {
+    public static void main(String[] args) {
+        int arr[] ={0,20,0,40,50,75};
+        int j = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+             int   temp =arr[i];
+                arr[i] = arr[j];
+                arr[j] =temp;
+                j++;
+             }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+}

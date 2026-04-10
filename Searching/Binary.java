188364b8b2fc25921218965aa76e6a121e19f435
Gourav554 <Jgourav554@gmail.com>
2026-04-10 21:05:36 +0530

    Binary

---
+++ Searching/Binary.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+public class Binary {
+    
+//     Sirf sorted array me kaam karta hai
+// Middle se check karta hai → half data eliminate
+// Fast (O(log n))
+
+public static void main(String[] args) {
+    int[] arr = {1, 2, 3, 4, 5};
+    int target = 5;
+    // int ans = binarySearch(arr, target);
+    // System.out.println(ans);
+
+    for(int i =0;i<arr.length;i++){
+        if()
+    }
+}
+}

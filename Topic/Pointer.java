7f1a9264cec37426a2afacbdb9f40ca5ca945844
Gourav554 <Jgourav554@gmail.com>
2026-04-11 23:24:47 +0530

    point

--- Topic/Pointer.java
+++ Topic/Pointer.java
@@ -3,6 +3,7 @@ class Student {
     int age;
 }
 //Durse ka variable store karne ke liye memory allocate karta hai, uske address ko pointer kehte hai. Pointer se hum us variable ko access kar sakte hai.
+//It is a Referance variable
 public class Pointer {
     public static void main(String[] args) {
         Student s1 = new Student();

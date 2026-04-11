df71eea8b69978aa278f4c41f597819df91b1730
Gourav554 <Jgourav554@gmail.com>
2026-04-11 22:54:17 +0530

    pointer

--- Topic/Pointer.java
+++ Topic/Pointer.java
@@ -1,5 +1,13 @@
 package Topic;
-
-public  class Pointer {
-    
+class Student {
+    int age;
 }
+
+public class Pointer {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.age = 20;
+        System.out.println(s1.age);
+    }
+}
+

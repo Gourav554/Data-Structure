371438966791b0ba370afb9195be79e829dd7ae2
Gourav554 <Jgourav554@gmail.com>
2026-04-12 20:13:37 +0530

    Insert

--- Linkedlist/Insert.java
+++ Linkedlist/Insert.java
@@ -3,15 +3,46 @@ package Linkedlist;
 public class Insert {
 
     class Node{
-        public int data;
-        public Node next;
+          Node head = null;
+
+          // Inser at the Begnning
+          public void insertAtBegnning(int data){
+            Node newNode = new Node(data);
+              if(head==null){
+                head = node;
+                return;
+              }
+                newNode.next = head.next;
+                head = newNode;
+
+          }
 
 
-        public Node (int data){
-            this.data = data;
-            this.next = null;
-        }
+//Insert at last
+public void insertAtlast(int data){
+
+    Node newNode = new Node(data);
+      if(head==null){
+        head = newNode;
+        return;
+      }
+      Node temp = head;
+      while(temp.next != null){
+        temp = temp.next;
+      }
+      temp.next = newNode;
+}
+
+
+
+//Insert in a Specific Position
+public void userAtPosition(int data, int Position
+    Node newNode = new Node(data);
+    if(Position ==2){
+        newNode.next  head;
+ 
     }
+)
  public static void main(String[] args) {
     
 

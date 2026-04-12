c2141326b763ccdad271275e9dd32550ea47d0e7
Gourav554 <Jgourav554@gmail.com>
2026-04-12 19:43:26 +0530

    node

--- Linkedlist/Insert.java
+++ Linkedlist/Insert.java
@@ -1,5 +1,19 @@
 package Linkedlist;
 
 public class Insert {
+
+    class Node{
+        public int data;
+        public Node next;
+
+
+        public Node (int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+ public static void main(String[] args) {
     
+
+ }   
 }

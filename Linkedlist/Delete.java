87c3991376ee3b1249feeb34c9c1ae9df2e4b4e1
Gourav554 <Jgourav554@gmail.com>
2026-04-12 21:18:31 +0530

    Delete

---
+++ Linkedlist/Delete.java
@@ -0,0 +1,16 @@
+package Linkedlist;
+
+public class Delete {
+    class Node{
+        int Data;
+        Node next;
+
+        Node(int data){
+            this.Data = data;
+            this.next = null;
+        }
+
+        
+    }
+    
+}

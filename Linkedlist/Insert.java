e51dbf31d10bbea5dbd11dd558de5c67374db694
Gourav554 <Jgourav554@gmail.com>
2026-04-12 20:27:42 +0530

    Inserrt

--- Linkedlist/Insert.java
+++ Linkedlist/Insert.java
@@ -2,49 +2,85 @@ package Linkedlist;
 
 public class Insert {
 
-    class Node{
-          Node head = null;
+    class Node {
+        int data;
+        Node next;
 
-          // Inser at the Begnning
-          public void insertAtBegnning(int data){
-            Node newNode = new Node(data);
-              if(head==null){
-                head = node;
-                return;
-              }
-                newNode.next = head.next;
-                head = newNode;
-
-          }
-
-
-//Insert at last
-public void insertAtlast(int data){
-
-    Node newNode = new Node(data);
-      if(head==null){
-        head = newNode;
-        return;
-      }
-      Node temp = head;
-      while(temp.next != null){
-        temp = temp.next;
-      }
-      temp.next = newNode;
-}
-
-
-
-//Insert in a Specific Position
-public void userAtPosition(int data, int Position
-    Node newNode = new Node(data);
-    if(Position ==2){
-        newNode.next  head;
- 
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
     }
-)
- public static void main(String[] args) {
-    
 
- }   
+    Node head = null;
+
+    // Insert at Beginning
+    public void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Insert at Last
+    public void insertAtLast(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+
+    // Insert at Specific Position
+    public void insertAtPosition(int data, int position) {
+        Node newNode = new Node(data);
+
+        if (position == 1) {
+            insertAtBeginning(data);
+            return;
+        }
+
+        Node temp = head;
+
+        for (int i = 1; i < position - 1; i++) {
+            if (temp == null) {
+                System.out.println("Out of bound");
+                return;
+            }
+            temp = temp.next;
+        }
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    // Print List
+    public void printList() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Insert list = new Insert();
+
+        list.insertAtBeginning(10);
+        list.insertAtBeginning(5);
+        list.insertAtLast(20);
+        list.insertAtPosition(15, 3);
+
+        list.printList();
+    }
 }

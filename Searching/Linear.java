8c87a53da4952aa0b8ce8415f27d4258cad84c8a
Gourav554 <Jgourav554@gmail.com>
2026-04-13 22:52:07 +0530

    linear

--- Searching/Linear.java
+++ Searching/Linear.java
@@ -3,9 +3,10 @@
 public class Linear {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
-        int target = 5;
+        int target = 2;
         // int ans = linearSearch(arr, target);
         // System.out.println(ans);
+
 
         for(int i =0 ;i<arr.length;i++){
             if(arr[i] == target){

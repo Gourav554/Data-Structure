ae05d2debbdd473344837d1b709dae7a39c782fc
Gourav554 <Jgourav554@gmail.com>
2026-04-14 21:29:55 +0530

    Search

--- Linkedlist/Search.java
+++ Linkedlist/Search.java
@@ -1,34 +1,38 @@
 package Linkedlist;
 
 public class Search {
+
     Node head;
 
-    class Node{
+    class Node {
         int data;
         Node next;
 
-        Node(int data){
+        Node(int data) {
             this.data = data;
             this.next = null;
         }
-Node head = null;
+    }
+
+
 
     // Insert at Beginning
     public void insertAtBeginning(int data) {
         Node newNode = new Node(data);
-
         newNode.next = head;
         head = newNode;
     }
 
 
-    //Search at node
-    public void search (int key){
+
+
+    // Search element (returns position)
+    public void searchAtPosition(int key) {
         Node temp = head;
         int position = 1;
 
-        while(temp != null){
-            if(temp.data == key){
+        while (temp != null) {
+            if (temp.data == key) {
                 System.out.println("Element found at position: " + position);
                 return;
             }
@@ -37,12 +41,33 @@ Node head = null;
         }
 
         System.out.println("Element not found in the list.");
-
     }
 
 
 
-    public
+
+    // Print list (optional)
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
     }
+
+
+
     
+    public static void main(String[] args) {
+        Search list = new Search(); // ✅ correct class name
+
+        list.insertAtBeginning(10);
+        list.insertAtBeginning(20);
+        list.insertAtBeginning(30);
+
+        list.printList();
+
+        list.searchAtPosition(20);
+    }
 }

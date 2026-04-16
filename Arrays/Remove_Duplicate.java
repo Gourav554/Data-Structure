c257e7a58ae9936bde3a6c06bf91aa72db0b331d
Gourav554 <Jgourav554@gmail.com>
2026-04-16 20:26:23 +0530

    duplicate

--- Arrays/Remove_Duplicate.java
+++ Arrays/Remove_Duplicate.java
@@ -1,3 +1,23 @@
-public class Remove_Duplicate {
-    
+
+import java.util.*;
+
+class Remove_Duplicate {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 2, 3};
+
+        Arrays.sort(nums);
+
+        int j = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[j]) {
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+
+        // Print unique elements
+        for (int i = 0; i <= j; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
 }

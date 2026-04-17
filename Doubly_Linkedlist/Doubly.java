a4056f9da0350a23cb1776b9342b03f721ed9074
Gourav554 <Jgourav554@gmail.com>
2026-04-17 23:16:07 +0530

    list

--- Doubly_Linkedlist/Doubly.java
+++ Doubly_Linkedlist/Doubly.java
@@ -18,7 +18,19 @@ public class Doubly {
         Node head;
 
         //Add First Method
-        public void addFirst(int data)
+        public void addFirst(int data){
+            Node newNode = new node(data);
+            if(head==null){
+                head = newNode;
+                return;
+            }
+            //Link new Node withj Head
+            newNode.next = head;
+            head.prev = newNode;
+
+            //Update Head to new Node
+
+        }
     }
     }
 }

b0322211854fe073748f4ac405e65bca8045a405
Gourav554 <Jgourav554@gmail.com>
2026-04-17 23:13:02 +0530

    dou

--- Doubly_Linkedlist/Doubly.java
+++ Doubly_Linkedlist/Doubly.java
@@ -2,10 +2,21 @@
 
 public class Doubly {
     public static void main(String[] args) {
-        
+        class Node{
         int data;
         Node next;
         Node prev;
+
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    class DoublyLinkedlist{
+        Node head;
         
+    }
     }
 }

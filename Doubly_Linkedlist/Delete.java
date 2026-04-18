dc4a86b6e35ca5962cadb53ae228e8dc39daa7e3
Gourav554 <Jgourav554@gmail.com>
2026-04-18 21:10:51 +0530

    delete

---
+++ Doubly_Linkedlist/Delete.java
@@ -0,0 +1,21 @@
+import org.w3c.dom.Node;
+
+public class Delete {
+    
+    class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    Node head;
+
+    //Delete first node
+    public 
+}

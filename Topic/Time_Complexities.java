63f7e30d7e84e5c2078a3353ec4b51048894b1ba
Gourav554 <Jgourav554@gmail.com>
2026-04-20 21:03:54 +0530

    time

--- Topic/Time_Complexities.java
+++ Topic/Time_Complexities.java
@@ -3,5 +3,7 @@ package Topic;
 public class Time_Complexities {
     
 }
+
+//Loop kam karo tho time comp
 // Time Complexity measures how much time an algorithm takes to run as the input size increases.
 // It helps us understand how efficient an algorithm is.

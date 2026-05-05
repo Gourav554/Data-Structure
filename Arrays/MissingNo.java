6a00fd41bb0863f07d9531c18c09dc5504084c65
Gourav554 <Jgourav554@gmail.com>
2026-05-05 22:45:30 +0530

    miss

--- Arrays/MissingNo.java
+++ Arrays/MissingNo.java
@@ -2,4 +2,11 @@ import java.util.*;
 public class MissingNo {
     public static void main(String args[])
     
+    {
+        int arr[] = {1, 2, 3, 5};
+        int n = arr.length + 1; // Total numbers from 1 to n
+        int sum = n * (n + 1) / 2; // Sum of first n natural numbers
+        int missingNumber = sum - Arrays.stream(arr).sum(); // Subtract the sum of array from total sum
+        System.out.println("Missing number: " + missingNumber);
+    }
 }
